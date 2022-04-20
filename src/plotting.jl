7626533cca600ec71c621bcbdad62883abeebd96
Kyle Matsuda <kyle.yoshio.matsuda@gmail.com>
2022-04-20 05:25:38 -0600

    finish rough go at plotting functions

--- src/plotting.jl
+++ src/plotting.jl
@@ -163,6 +163,91 @@ function plot_states_adiabatic(
     return f
 end
 
+function plot_states_adiabatic_weighted(
+    spectra,
+    states::Vector{State};
+    groupby=:E,
+    radius::Union{Int,Nothing}=nothing 
+)
+    adiabatized = connect_adiabatically(spectra;
+        groupby=groupby,
+        radius=radius
+    )
+
+    if groupby == :E
+        xlabel = "E (V/cm)"
+    elseif groupby == :B
+        xlabel = "B (G)"
+    else
+        xlabel = "$groupby"
+    end
+
+    f = Figure(
+        fontsize = 14,
+        font = "Helvetica",
+        resolution = (300, 450),
+    )
+
+    ax = Axis(
+        f[1,1],
+        xlabel=xlabel,
+        ylabel="Energy (MHz)",
+        backgroundcolor = :gray85,
+    )
+
+    cmap = :deep
+    colorrange = (-4, 0)
+
+    max_weight(ev) = maximum([
+        ev[state_to_index(s)] |> abs2
+        for s in states
+    ])
+
+    DataFrames.transform!(adiabatized,
+        :eigenstate => DataFrames.ByRow(max_weight) => :max_weight
+    )
+
+    function add_sup_weight(df)
+        DataFrames.sort!(df, :max_weight)
+        sup_weight = last(df).max_weight
+        DataFrames.transform!(df,
+            :index => DataFrames.ByRow(x -> sup_weight) => :sup_weight
+        )
+        return df
+    end
+
+    by_sup_weight = transform_spectra(
+        adiabatized,
+        add_sup_weight;
+        groupby=:adiabatic_index
+    )
+
+
+    groups = DataFrames.groupby(
+        by_sup_weight,
+        :sup_weight;
+        sort=true
+    )
+
+    for g in groups
+        DataFrames.sort!(g, groupby)
+
+        lines!(
+            ax,
+            g[!, groupby],
+            g.energy,
+            linewidth=2,
+            color=log10.(g.max_weight),
+            colormap=cmap,
+            colorrange=colorrange
+        )
+    end
+
+    Colorbar(f[1, 2], limits=colorrange, colormap=cmap, label="log10(weight)")
+
+    return f
+end
+
 function plot_transitions_adiabatic(
     spectra,
     hamiltonian_parts::HamiltonianParts,
@@ -208,7 +293,7 @@ function plot_transitions_adiabatic(
         f[1,1],
         xlabel=xlabel,
         ylabel="Energy (MHz)",
-        backgroundcolor = :gray75,
+        backgroundcolor = :gray85,
     )
 
     function add_max_strength(df)
@@ -238,8 +323,6 @@ function plot_transitions_adiabatic(
     for g in groups
         DataFrames.sort!(g, groupby)
 
-        println(last(g).max_strength)
-
         lines!(
             ax,
             g[!, groupby],
@@ -255,153 +338,3 @@ function plot_transitions_adiabatic(
     
     return f
 end
-
-# function plot_transitions_vs_E(
-#     hamiltonian_parts::HamiltonianParts,
-#     fields_scan::Vector{ExternalFields},
-#     g::State,
-#     frequency_range::Union{Vector,Nothing} = nothing;
-#     tol = 0.5,
-#     use_cutoff = false,
-#     cutoff = 1e-6,
-# )
-#     spectra = calculate_transitions_vs_E(
-#         hamiltonian_parts,
-#         fields_scan,
-#         g,
-#         frequency_range;
-#         tol = tol,
-#         use_cutoff = use_cutoff,
-#         cutoff = cutoff,
-#     )
-#     return plot_transitions_vs_E(spectra)
-# end
-
-# function plot_transitions_vs_E(spectra)
-#     f = Figure(fontsize = 18)
-#     ax = Axis(
-#         f[1, 1],
-#         backgroundcolor = :gray75,
-#         xlabel = "E (V/cm)",
-#         ylabel = "Frequency (MHz)",
-#     )
-
-#     df = DataFrames.groupby(spectra, :index)
-
-#     cmap = :deep
-#     colorrange = (-4, 0)
-
-#     for group in df
-#         DataFrames.sort!(group, :E)
-#         lines!(
-#             group.E,
-#             group.transition_frequency,
-#             color = log10.(group.transition_strength),
-#             colormap = cmap,
-#             colorrange = colorrange,
-#         )
-#     end
-
-#     Colorbar(f[1, 2], limits = colorrange, colormap = cmap, label = "log10(strength)")
-
-#     return f
-# end
-
-
-
-# function plot_chi_vs_E(
-#     hamiltonian_parts::HamiltonianParts,
-#     fields_scan::Vector{ExternalFields},
-#     g::State;
-#     p = 0,
-#     tol = 0.5,
-# )
-#     chis = calculate_chi_vs_E(hamiltonian_parts, fields_scan, g; p = p, tol = tol)
-#     plot_chi_vs_E(chis)
-# end
-
-# function plot_chi_vs_E(spectra)
-#     f = Figure(fontsize = 18)
-#     ax = Axis(f[1, 1], xlabel = "E (V/cm)", ylabel = L"$\chi / d_p^2$")
-
-#     df = DataFrames.groupby(spectra, :basis_index)
-
-#     for group in df
-#         DataFrames.sort!(group, :E)
-#         N = first(group.N)
-#         m_n = first(group.m_n)
-
-#         lines!(group.E, real.(group.chi), label = "$N, $m_n")
-#     end
-
-#     f[1, 2] = Legend(f, ax, "States", framevisible = true)
-
-#     return f
-# end
-
-# function plot_states_vs_E(
-#     hamiltonian_parts::HamiltonianParts,
-#     fields_scan::Vector{ExternalFields},
-#     states::Vector{State};
-#     redraw_threshold = 0.2,
-# )
-#     Ns = [s.N for s in states]
-#     filter_Ns(df) = DataFrames.filter(:N => n -> n in Ns, df)
-
-#     addE(df) = DataFrames.transform(df, :fields => (f -> map(g -> g.E.magnitude, f)) => :E)
-
-#     spectra = calculate_spectra_vs_fields(hamiltonian_parts, fields_scan, addE ∘ filter_Ns)
-#     return plot_states_vs_E(spectra, states; redraw_threshold = redraw_threshold)
-# end
-
-# function plot_states_vs_E(spectra, states; redraw_threshold = 0.2)
-#     f = Figure(fontsize = 18, resolution = (800, 600))
-#     ax = Axis(f[1, 1], xlabel = "E (V/cm)", ylabel = "Frequency (MHz)")
-
-#     cmap = :deep
-#     colorrange = (-0.05, 1)
-
-#     max_weight(ei) = maximum([abs2.(ei)[state_to_index(s)] for s in states])
-#     df = DataFrames.transform(
-#         spectra,
-#         :eigenstate => (e -> map(max_weight, e)) => :max_weight,
-#     )
-
-#     grouped = DataFrames.groupby(df, :index)
-#     redraw = []
-#     for (i, group) in enumerate(grouped)
-#         DataFrames.sort!(group, :E)
-
-#         if maximum(group.max_weight) > redraw_threshold
-#             append!(redraw, i)
-#         end
-
-#         lines!(
-#             group.E,
-#             group.energy,
-#             color = group.max_weight,
-#             colormap = cmap,
-#             colorrange = colorrange,
-#             transparency = false,
-#             overdraw = false,
-#         )
-#     end
-
-#     println("poop")
-
-#     for i in redraw
-#         group = grouped[i]
-#         lines!(
-#             group.E,
-#             group.energy,
-#             color = group.max_weight,
-#             colormap = cmap,
-#             colorrange = colorrange,
-#             transparency = false,
-#             overdraw = true,
-#             fxaa = true,
-#         )
-#     end
-
-#     return f
-# end
